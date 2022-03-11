0977e59145d9229e5eac2de61ad42ef729f8190d
Aaditya Bansal <66950964+aadi753@users.noreply.github.com>
2022-03-11 23:26:22 +0530

    Add files via upload

--- SUB_XOR.cpp
+++ SUB_XOR.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <string>
-#include <vector>
+// #include <vector>
+#include<cmath>
 //max val of long 9223372036854775807
 //max val of int 147483647
 using namespace std;
@@ -9,38 +10,41 @@ int main(){
 	int t;
 	cin >> t;
 	while(t--){
-		int n;
+		long n;
 		cin >> n;
 
 		string s;
 		cin >> s;
 
-		std::vector<char> arr;
-		arr.assign(n, '0');
-		
-		for(int i = n; i > 0; i--){
-			for(int j = 0; j <= n - i; j++){
-				string sub = s.substr(j, i);
-
-				int w = n - 1;
-				for(int k = sub.size(); i >= 0; i--){
-					if(arr[w] == sub[k]){
-						arr[w] = '0';
+		int ans[n];
+		for(long i = 0; i < n; i++){
+			int flag = 0;
+			for(long j = 0; j < i + 1; j = j + 2){
+				if(s[j] == '1'){
+					if(flag == 0){
+						flag = 1;
 					}
 					else{
-						arr[w] = '1';
+						flag = 0;
 					}
-
-					w--;
 				}
+			}
+
+			if(flag == 0){
+				ans[i] = 1;
+			}
+			else{
+				ans[i] = 0;
 			}
 		}
 
-		for(int i = 0; i < n; i++){
-			cout << arr[i];
-		}	
+		long answer = 0;
+		for(long i = 0; i < n; i++){
+		    if(ans[i] == 1)
+			answer += pow(2, n - i - 1);
+		}
 
-		cout << endl;
+		cout << answer <<  endl;
 	}
 	return 0;
 }
