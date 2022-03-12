85477c8ddd9720d6b6485909efd8e50e2cc56588
Aaditya Bansal <66950964+aadi753@users.noreply.github.com>
2022-03-12 17:26:07 +0530

    The Basic Template

---
+++ 00.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+// #include <bits/stdc++.h>;
+// #include <string>
+// #include <vector>
+// #define LONG_MAX 9223372036854775807;
+// #define INT_MAX 147483647;
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+	}
+	return 0;
+}
