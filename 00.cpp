13b9eb32a073f3b07797d2fdb0810440e3606f72
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2022-03-18 23:46:05 +0530

    The Basic Template
    starter code

--- 00.cpp
+++ 00.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
-// #include <bits/stdc++.h>;
-// #include <string>
+#include <bits/stdc++.h>
 // #include <vector>
 // #define LONG_MAX 9223372036854775807;
 // #define INT_MAX 147483647;
@@ -12,6 +11,8 @@ int main(){
 	while(t--){
 		int n;
 		cin >> n;
+
+		cout << endl;
 	}
 	return 0;
 }
