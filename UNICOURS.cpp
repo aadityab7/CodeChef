164f8eefac31d697a82dab038b88cfc0dd900424
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2022-06-28 18:19:03 +0530

    28-06-22 Practice
    L 2 - beg

---
+++ UNICOURS.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <bits/stdc++.h>
+// #include <vector>
+// #define LONG_MAX 9223372036854775807;
+// #define INT_MAX 147483647;
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+
+		int num;
+        int max = 0;
+        
+		for(int i = 0; i < n; i++){
+			cin >> num;
+			if(num > max){
+				max = num;
+			}
+		}
+
+		int ans = n - max;
+		cout << ans << endl;
+	}
+	return 0;
+}
