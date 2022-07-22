971ad15caa6983a1a05de465b1c4a0b702af8c30
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2022-07-22 21:32:44 +0530

    22-07-22 july long
    div - 4

---
+++ BURGERS2.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include <bits/stdc++.h>
+// #include <vector>
+// #define LONG_MAX 9223372036854775807;
+// #define INT_MAX 147483647;
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	long t;
+	cin >> t;
+	while(t--){
+		int x, y;
+		long n;
+		long long r;
+
+		cin >> x >> y >> n >> r;
+
+		//long count = 0;
+		long pre = r / y;
+
+		if((r / x) < n){
+			cout << -1 << "\n";
+		}
+		else if(pre >= n){
+			cout << n - pre << ' ' << pre << "\n";
+		}
+		else{
+			//current money spent = n * x;
+			//if we buy a premium burger it will be (n - count) * x + count * y <= r
+
+			// 			while((count <= n) && (((x * (n - count)) + (y * count)) <= r)){
+			// 				count++;
+			// 			}
+            
+
+        	long long cost_pre = y * pre;
+        	long long cost_nor = x * (n - pre);
+
+            while(pre >= 0 && ((cost_nor + cost_pre) > r)){
+				pre--;
+				cost_nor += x;
+				cost_pre -= y;
+			}
+        
+			cout << n - pre << ' ' << pre << "\n";
+
+			//cout << n - (count - 1) << ' ' << count - 1 << "\n";
+		}	
+	}
+	return 0;
+}
