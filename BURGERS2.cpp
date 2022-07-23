bfacc5478121886118cc77b882b5f0a9d957a2e3
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2022-07-23 22:01:44 +0530

    23-07-22 July Long
    Div 4

--- BURGERS2.cpp
+++ BURGERS2.cpp
@@ -16,38 +16,52 @@ int main(){
 		long long r;
 
 		cin >> x >> y >> n >> r;
-
-		//long count = 0;
-		long pre = r / y;
-
+		
 		if((r / x) < n){
 			cout << -1 << "\n";
 		}
-		else if(pre >= n){
-			cout << n - pre << ' ' << pre << "\n";
-		}
 		else{
-			//current money spent = n * x;
-			//if we buy a premium burger it will be (n - count) * x + count * y <= r
+			long long premium_burgers = (r - (n * x)) / (y - x);
 
-			// 			while((count <= n) && (((x * (n - count)) + (y * count)) <= r)){
-			// 				count++;
-			// 			}
+			if(premium_burgers > n){
+				premium_burgers = n;
+			}
+
+			cout << n - premium_burgers << ' ' << premium_burgers << "\n"; 
+		}
+		
+
+		// //long count = 0;
+		// long pre = r / y;
+
+		// if((r / x) < n){
+		// 	cout << -1 << "\n";
+		// }
+		// else if(pre >= n){
+		// 	cout << n - pre << ' ' << pre << "\n";
+		// }
+		// else{
+		// 	//current money spent = n * x;
+		// 	//if we buy a premium burger it will be (n - count) * x + count * y <= r
+
+		// 	// 			while((count <= n) && (((x * (n - count)) + (y * count)) <= r)){
+		// 	// 				count++;
+		// 	// 			}
             
 
-        	long long cost_pre = y * pre;
-        	long long cost_nor = x * (n - pre);
+  //       	long long cost_pre = y * pre;
+  //       	long long cost_nor = x * (n - pre);
 
-            while(pre >= 0 && ((cost_nor + cost_pre) > r)){
-				pre--;
-				cost_nor += x;
-				cost_pre -= y;
-			}
+  //           while(pre >= 0 && ((cost_nor + cost_pre) > r)){
+		// 		pre--;
+		// 		cost_nor += x;
+		// 		cost_pre -= y;
+		// 	}
         
-			cout << n - pre << ' ' << pre << "\n";
+		// 	cout << n - pre << ' ' << pre << "\n";
 
-			//cout << n - (count - 1) << ' ' << count - 1 << "\n";
-		}	
+		// 	//cout << n - (count - 1) << ' ' << count - 1 << "\n";
+		// }	
 	}
 	return 0;
 }
